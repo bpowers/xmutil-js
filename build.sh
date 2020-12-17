15fa584511a7cf184c82d01724fe2746418cadf0
Bobby Powers <bobbypowers@gmail.com>
2020-12-16 22:04:29 -0800

    enable optimizations for emscripten generated code + remove fs

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ mkdir -p build
 cd build
 emcmake cmake -GNinja -DCMAKE_BUILD_TYPE=Release ../third_party/xmutil
 ninja
-em++ -o xmutil.js -O0 -s MODULARIZE=1 -s 'EXPORT_NAME="createModule"' -s EXPORTED_FUNCTIONS='["__convert_mdl_to_xmile"]' -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]' libxmutil.a libutf.a libtinyxml2.a
+em++ -o xmutil.js -O2 -s FILESYSTEM=0 -s MODULARIZE=1 -s 'EXPORT_NAME="createModule"' -s EXPORTED_FUNCTIONS='["__convert_mdl_to_xmile"]' -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]' libxmutil.a libutf.a libtinyxml2.a
 
 cp xmutil.js ../src/
 cp xmutil.wasm ../src/
