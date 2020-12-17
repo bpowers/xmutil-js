fe5dd2c57d84691e249c059bf85430b452914d12
Bobby Powers <bobbypowers@gmail.com>
2020-12-16 21:42:49 -0800

    it works!

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 // Copyright 2020 Bobby Powers. All rights reserved.
 // Use of this source code is governed by the MIT License
 // that can be found in the LICENSE file.
+
+import createModule from './xmutil';
+
+export async function convertMdlToXmile(mdlSource: string | Readonly<Uint8Array>, pretty = true): Promise<string> {
+  const xmutil = await createModule();
+  return xmutil.ccall(
+    '_convert_mdl_to_xmile',
+    'string',
+    ['string', 'number', 'boolean'],
+    [mdlSource, mdlSource.length, !pretty],
+  );
+}
