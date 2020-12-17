fee02370112cefbe41a4c17d039497d2dacd53ad
Bobby Powers <bobbypowers@gmail.com>
2020-12-16 20:29:46 -0800

    add build script and proejct.json

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+rm -rf build
+mkdir -p build
+cd build
+emcmake cmake -GNinja -DCMAKE_BUILD_TYPE=Release ../third_party/xmutil
+ninja
+em++ -o xmutil.js -O3 -s EXPORTED_FUNCTIONS='["__convert_mdl_to_xmile"]' -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]' libxmutil.a libutf.a libtinyxml2.a
+
+cp xmutil.js ../src/
+cp xmutil.wasm ../src/
