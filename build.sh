901f07f76b7de5a7344d7fa6521a1cf8b1a7c6ba
Bobby Powers <bobbypowers@gmail.com>
2020-12-17 12:13:07 -0800

    ditch the emscripten wrapper

--- build.sh
+++ build.sh
@@ -6,7 +6,6 @@ mkdir -p build
 cd build
 emcmake cmake -GNinja -DCMAKE_BUILD_TYPE=Release ../third_party/xmutil
 ninja
-em++ -o xmutil.js -O2 -s FILESYSTEM=0 -s MODULARIZE=1 -s 'EXPORT_NAME="createModule"' -s EXPORTED_FUNCTIONS='["__convert_mdl_to_xmile"]' -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]' libxmutil.a libutf.a libtinyxml2.a
+em++ -o xmutil.wasm -Os --no-entry -s STANDALONE_WASM -s FILESYSTEM=0 -s MODULARIZE=1 -s 'EXPORT_NAME="createModule"' -s EXPORTED_FUNCTIONS='["_free", "_malloc", "__convert_mdl_to_xmile"]' -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]' libxmutil.a libutf.a libtinyxml2.a
 
-cp xmutil.js ../src/
 cp xmutil.wasm ../src/
--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,41 @@
 // Use of this source code is governed by the MIT License
 // that can be found in the LICENSE file.
 
-import createModule from './xmutil';
+import * as wasm from './xmutil.wasm';
 
-export async function convertMdlToXmile(mdlSource: string | Readonly<Uint8Array>, pretty = true): Promise<string> {
-  const xmutil = await createModule();
-  return xmutil.ccall(
-    '_convert_mdl_to_xmile',
-    'string',
-    ['string', 'number', 'boolean'],
-    [mdlSource, mdlSource.length, !pretty],
-  );
+const cachedTextEncoder = new TextEncoder();
+const cachedTextDecoder = new TextDecoder();
+
+let cachegetUint8Memory0: Uint8Array | null = null;
+function getUint8Memory0() {
+  if (cachegetUint8Memory0 === null || cachegetUint8Memory0.buffer !== wasm.memory.buffer) {
+    cachegetUint8Memory0 = new Uint8Array(wasm.memory.buffer);
+  }
+  return cachegetUint8Memory0;
+}
+
+function getStringFromWasm(ptr: number) {
+  const mem = getUint8Memory0();
+  let off = 0;
+  while (mem[ptr + off] !== 0) {
+    off++;
+  }
+  return getUint8Memory0().subarray(ptr / 1, ptr / 1 + off);
+}
+
+export function convertMdlToXmile(mdlSource: string | Readonly<Uint8Array>, pretty = true): string {
+  if (typeof mdlSource === 'string') {
+    mdlSource = cachedTextEncoder.encode(mdlSource);
+  }
+
+  const mdlSourcePtr = wasm.malloc(mdlSource.length);
+  getUint8Memory0()
+    .subarray(mdlSourcePtr, mdlSourcePtr + mdlSource.length)
+    .set(mdlSource);
+
+  const resultPtr = wasm._convert_mdl_to_xmile(mdlSourcePtr, mdlSource.length, !pretty);
+  const resultBuf = getStringFromWasm(resultPtr);
+  wasm.free(resultPtr);
+
+  return cachedTextDecoder.decode(resultBuf);
 }
