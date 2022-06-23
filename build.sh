b3a0a9cf130b4db632b09b64fad60fc5997161f6
Bobby Powers <bobbypowers@gmail.com>
2022-06-23 15:32:58 -0700

    bump to latest, but problems with wanting wasi imports

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ mkdir -p build
 cd build
 emcmake cmake -GNinja -DCMAKE_BUILD_TYPE=Release ../third_party/xmutil
 ninja
-em++ -o xmutil.wasm -Os --no-entry -s DYNAMIC_EXECUTION=0 -s STANDALONE_WASM -s FILESYSTEM=0 -s MODULARIZE=1 -s 'EXPORT_NAME="createModule"' -s EXPORTED_FUNCTIONS='["_free", "_malloc", "__convert_mdl_to_xmile"]' -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]' libxmutil.a libutf.a libtinyxml2.a
+em++ -o xmutil.wasm -Os -g3 -fwasm-exceptions -DNDEBUG --no-entry -s PURE_WASI=1 -s STRICT=1 -s ASSERTIONS=0 -s DYNAMIC_EXECUTION=0 -s STANDALONE_WASM=1 -s FILESYSTEM=0 -s NO_RELOCATABLE -s EXPORTED_FUNCTIONS='["_free", "_malloc", "_xmutil_convert_mdl_to_xmile", "_xmutil_get_log", "_xmutil_clear_log"]' -s EXPORTED_RUNTIME_METHODS='[]' libxmutil.a libutf.a libtinyxml2.a
 
 cp xmutil.wasm ../src/
