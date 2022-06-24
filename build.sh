dd57f101cfc80a822ebfc0300bd87870ac406f8e
Bobby Powers <bobbypowers@gmail.com>
2022-06-23 18:25:42 -0700

    build without needing imports

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ mkdir -p build
 cd build
 emcmake cmake -GNinja -DCMAKE_BUILD_TYPE=Release ../third_party/xmutil
 ninja
-em++ -o xmutil.wasm -Os -g3 -DNDEBUG --no-entry -s DISABLE_EXCEPTION_THROWING=1 -s PURE_WASI=1 -s STRICT=1 -s ASSERTIONS=0 -s DYNAMIC_EXECUTION=0 -s SUPPORT_LONGJMP=0 -s STANDALONE_WASM=1 -s FILESYSTEM=0 -s NO_RELOCATABLE -s EXPORTED_FUNCTIONS='["_free", "_malloc", "_xmutil_convert_mdl_to_xmile", "_xmutil_get_log", "_xmutil_clear_log"]' -s EXPORTED_RUNTIME_METHODS='[]' libxmutil.a libutf.a libtinyxml2.a
+em++ -o xmutil.wasm -Os -DNDEBUG --no-entry -s DISABLE_EXCEPTION_THROWING=1 -s PURE_WASI=1 -s STRICT=1 -s ASSERTIONS=0 -s DYNAMIC_EXECUTION=0 -s SUPPORT_LONGJMP=0 -s STANDALONE_WASM=1 -s FILESYSTEM=0 -s NO_RELOCATABLE -s EXPORTED_FUNCTIONS='["_free", "_malloc", "_xmutil_convert_mdl_to_xmile", "_xmutil_get_log", "_xmutil_clear_log"]' -s EXPORTED_RUNTIME_METHODS='[]' libxmutil.a libutf.a libtinyxml2.a
 
 cp xmutil.wasm ../src/
